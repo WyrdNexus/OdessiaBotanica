43a4713ed9306d0f7406e0be7d75288dd4fc3874
Tony Chiboucas <WyrdTony@gmail.com>
2017-07-22 03:15:32 -0700

    environment setup + code copyover + inital framing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,16 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle();
+            $bundles[] = new Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle();
+            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
--- docs/REQUIREMENTS.md
+++ docs/REQUIREMENTS.md
@@ -1,7 +1,30 @@
 # Project Requirements
 
-* php 7.1+
-* git
-* mariadb
-* symfony 3.2+
-* composer
+* [php](http://php.net/downloads.php) 7.1+
+* [git](https://git-scm.com/downloads)
+* [mariadb](https://mariadb.org/)
+* [composer](https://getcomposer.org/download/)
+* [symfony](https://symfony.com/) 3.3+
+    * [webpack-encore](https://symfony.com/doc/current/frontend.html)
+        * [node.js](https://nodejs.org/en/download/) 6.11+
+        * [yarn](https://yarnpkg.com/lang/en/docs/install/) 0.27.5+
+    * [vue](https://vuejs.org)
+
+## PHP 7.1+
+
+With the following adjustments to php.ini:
+* realpath_cache_size = 5M
+
+## Webpack and Vue
+The following must be installed in a specific order:
+1. Node.js
+2. Yarn
+3. webpack-encore
+4. vue
+
+Node.js and Yarn should install fairly simply.
+(Windows: logout and login for an updated PATH )
+
+### Webpack
+
+    > yarn add @symfony/webpack-encore --dev 
