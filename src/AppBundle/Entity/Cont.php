4ec9ac17d7c8d01f4a3502ad823af1b9be9d1e2d
Tony Chiboucas <WyrdTony@gmail.com>
2017-07-22 03:21:27 -0700

    initial config complete: node, yarn, webpack, mysql, fosUserBundle

---
+++ src/AppBundle/Entity/Cont.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * cont
+ */
+class Cont extends BaseEntity
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="conts")
+     */
+    private $author;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return cont
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return cont
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+}
+
